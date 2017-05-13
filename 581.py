86837d7ea17c362783defa38a998607c4250a238
root <root@localhost.localdomain>
2017-05-14 07:44:23 +0800

    x

---
+++ 581.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findUnsortedSubarray(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        start=end=-1
+        dup=sorted(nums)
+        for i in xrange(len(nums)):
+            if nums[i]!=dup[i]:
+                if start==-1: start=i
+                end=i
+        if end==start: return 0
+        return end-start+1
+s=Solution()
