421f9685e289255088b54590cc0fd8ac991c3b46
root <root@localhost.localdomain>
2017-05-13 08:29:04 +0800

    first

---
+++ 496.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def nextGreaterElement(self, findNums, nums):    
+        ans=[]
+        for num in findNums:
+            i = nums.index(num)
+            while i <len(nums):
+                if num < nums[i]:
+                    ans.append(nums[i])
+                    break
+                i+=1
+            if i==len(nums): ans.append(-1)
+        return ans
+s = Solution()
+print repr(s.nextGreaterElement([4,1,2],[1,3,4,2]))
