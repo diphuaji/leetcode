a61b0223180a17188ad6137bf19b04e99846b0c4
root <root@localhost.localdomain>
2017-05-21 14:57:04 +0800

    x

---
+++ 461.py
@@ -0,0 +1,9 @@
+#461.Hamming Distance
+class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        return sum([int(x) for x in bin(x^y) if x=='1'])
