8947e10bb0f5e35d4fd95cee8b91e12a242fa032
root <root@localhost.localdomain>
2017-05-30 09:05:03 +0800

    x

---
+++ 118.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        ans=[]
+        if numRows==0:
+            return ans
+        ans.append([1])
+        if numRows==1:
+            return ans
+        ans.append([1,1])
+        if numRows==2:
+            return ans
+        for i in xrange(2,numRows):
+            row=[1]
+            for j in xrange(i-1):
+                row.append(ans[i-1][j]+ans[i-1][j+1])
+            row.append(1)
+            ans.append(row)
+        return ans
---
+++ 169.py
@@ -0,0 +1,20 @@
+'''
+169. Majority Element 
+'''
+class Solution(object):
+    def majorityElement(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count=1
+        maj=nums[0]
+        for i in xrange(1,len(nums)):
+            if count==0:
+                count+=1
+                maj=nums[i]
+            elif nums[0]==maj:
+                count+=1
+            else:
+                count-=1
+        return maj
