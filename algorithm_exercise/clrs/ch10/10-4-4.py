1cee1a3226d62edc9574fa11093eb4fd04c8b929
Tianpeng Xia <xtpeqii@hotmail.com>
2017-07-11 01:26:05 +0800

    下
    X

---
+++ algorithm_exercise/clrs/ch10/10-4-4.py
@@ -0,0 +1,2 @@
+def printTree(node):
+    pass
--- algorithm_exercise/clrs/ch10/tree.py
+++ algorithm_exercise/clrs/ch10/tree.py
@@ -1,11 +1,19 @@
-class BTree:
+class Tree:
     def __init__(self):
         self.root=None
+class NewNode:
+    def __init__(self):
+        self.p=self.left=self.sib=None
+class BTree:
+    def __init__(self):
+        self.root=None    
 class Node:
     def __init__(self,key):
         self.left=self.right=self.p=None
         self.key=key
-data=[[1,12,7,3],
+
+def buildTree():
+    data=[[1,12,7,3],
     [2,15,8,None],
     [3,4,10,None],
     [4,10,5,9],
@@ -15,8 +23,6 @@ data=[[1,12,7,3],
     [8,14,6,2],
     [9,21,None,None],
     [10,5,None,None]]
-def buildTree():
-    global data
     tree =BTree()
     nodeDic={}
     for item in data:
@@ -41,8 +47,27 @@ def buildTree():
                 node.right=nodeDic[item[3]]=Node(data[item[3]-1][1])
             node.right.p=node
     return tree
+
+def buildRootedTree():
+    '''
+    [self_key,left_key,sib_key]
+    '''
+    data=[
+    [18,1,None],
+    [12,3,2],
+    [10,6,None],
+    [19,None,4],
+    [7,8,5],
+    [4,None,None],
+    [2,None,7],
+    [21,None,None],
+    [13,None,9],
+    [30,None,10],
+    [5,None,None]
+    ]
+    nodeDic={}
+    
+    
 if __name__=='__main__':
     tree = buildTree()
-    print tree.root.p
-    print tree.root.left
-    print tree.root.right
+    rootedTree=buildRootedTree()
