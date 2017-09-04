358fc3f6c878b2a7385bee5e5e89738d5b9af490
diphuaji <xtpeqii@hotmail.com>
2017-09-04 16:33:17 +0800

    update the problems of chapter 6

--- algorithm_exercise/clrs/ch6/p6-2.py
+++ algorithm_exercise/clrs/ch6/p6-2.py
@@ -1,9 +1,15 @@
 class DHeap:
-	def __init__(self,d=3):
-		self.heap_size=0
-		self._array=[]
-		self._d=d
-	def _next(self,i):
-		
-	def build_heap():
-		pass
+    def __init__(self,d=3):
+        self.heap_size=0
+        self._array=[]
+        self._d=d
+    def _parent(self,i):
+        return math.ceil((i-1)/self._d)
+    def _hasNext(self,i):
+        return i!=self.heap_size and (i-1)%self._d!=0
+    def _heapify(self,i):
+        lastLargest=None
+        largest=i
+        val=A[i]
+    def build_heap():
+        pass
